a9d7d41387ecab01302976e9e6f589466c39748e
Manuela <manuelamunozlujan0@gmail.com>
2025-03-18 09:54:41 -0500

    creando vista principal

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect
+from biblioteca import Biblioteca, Libro, Usuario, Prestamo
+
+app = Flask(__name__) #Creamos instancia de la aplicación Flask
+biblioteca = Biblioteca() #Creamos instancia de la clase biblioteca
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
